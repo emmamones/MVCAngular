9ffd47d7565d551fc75cc8707d80f7553eeccfb4
punky <punky_casinotomo@hotmail.com>
2016-08-18 23:02:49 +0200

    Create Factory servicios Movies CRUD operations

--- AngularMVCAuthentication/myapp.js
+++ AngularMVCAuthentication/myapp.js
@@ -1,18 +1,49 @@
 ﻿var app = angular.module("myApp", []);
 
-app.controller('GenreController', function ($scope, $http) {
+app.factory('crudServiceMovies', function ($http) {
 
-    $scope.GetGenres = function () {
-        $http.get('/Movies/Genres').then(function (response) {
-            $scope.modelGenre = response.data;
+    var crudMovies = {};
+
+    crudMovies.getGenres = function () {
+        var genres;
+        genres = $http({ method: 'Get', url: '/Movies/Genres' })
+            .then(function (response) {
+                return response.data;
+            });
+        return genres;
+    };
+
+    crudMovies.getMoviesByGenre = function (optGenre) {
+        var genres;
+        genres = $http({ method:'Get',url:'/Movies/ByGenre',params:{id:optGenre}})
+            .then(function (response) {
+            return response.data;
+        });
+        return genres;
+    };
+
+    crudMovies.editMovie = function (optGenre) {
+        $http.get('/Movies/Edit/' + optGenre).then(function (response) {
+            return response.data;
         });
     };
 
-    $scope.GetGenres();
+    return crudMovies;
 });
 
 
-app.controller('MoviesController', function ($scope, $http) {
+app.controller('GenreController', function ($scope, crudServiceMovies) {
+
+
+    crudServiceMovies.getGenres().then(function (result) {
+        $scope.modelGenres = result;
+    })
+
+});
+
+
+
+app.controller('MoviesController', function ($scope, crudServiceMovies) {
 
     $scope.Sort = function (column) {
         $scope.key = column;
@@ -20,20 +51,16 @@ app.controller('MoviesController', function ($scope, $http) {
     }
 
     $scope.GetMoviesByGenre = function (optGenre) {
-    
-        $http.get('/Movies/ByGenre/' + optGenre).then(function (response) {
-            $scope.Refresh;
-            $scope.modelMovies = response.data;
-        });
-    };
 
-   
-     $scope.Edit = function (idMovie) {
-    
-         $http.get('/Movies/Edit/' + idMovie).then(function(response) {
-            $scope.modelMovie = response.data;
-        });
+        crudServiceMovies.getMoviesByGenre(optGenre).then(function (result) {
+            $scope.modelMovies = result;
+        })
+    }
+
+
+    $scope.Edit = function (idMovie) {
+
+        $scope.modelMovie = crudServiceMovies.editMovie(idMovie);
     };
- 
 
 });
